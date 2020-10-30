b8e7fcccb7f114a72aa10bd1990fc1ca0457142e
Stefann11 <santrac.stefan@gmail.com>
2020-10-30 13:31:35 +0100

    feat: add feedback dto and adapter

---
+++ PatientWebApplication/PatientWebApplication/Adapters/FeedbackAdapter.cs
@@ -0,0 +1,30 @@
+﻿using PatientWebApplication.Dtos;
+using PatientWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientWebApplication.Adapters
+{
+    public class FeedbackAdapter
+    {
+        public static Feedback FeedbackDtoToFeedback(FeedbackDto dto)
+        {
+            Feedback feedback = new Feedback();
+            feedback.Message = dto.Message;
+            feedback.IsAnonymous = dto.IsAnonymous;
+            feedback.IsPublic = dto.IsPublic;
+            return feedback;
+        }
+
+        public static FeedbackDto FeedbackToFeedbackDto(Feedback feedback)
+        {
+            FeedbackDto dto = new FeedbackDto();
+            dto.Message = feedback.Message;
+            dto.IsAnonymous = feedback.IsAnonymous;
+            dto.IsPublic = feedback.IsPublic;
+            return dto;
+        }
+    }
+}
---
+++ PatientWebApplication/PatientWebApplication/Dtos/FeedbackDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientWebApplication.Dtos
+{
+    public class FeedbackDto
+    {
+        public String Message { get; set; }
+        public Boolean IsPublic { get; set; }
+        public Boolean IsAnonymous { get; set; }
+
+        public FeedbackDto() { }
+    }
+}
