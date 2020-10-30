342a1ea2b6719a50bf4da9df1eafb97e650f5057
Aleksa Santrač <akimbo.santrac@gmail.com>
2020-10-30 23:47:00 +0100

    feat: start ObservePublishedFeedback feature
    Created method for getting all published  feedback in controller
    Created method for getting all published  feedback in service

--- PatientWebApplication/PatientWebApplication/Controllers/FeedbackController.cs
+++ PatientWebApplication/PatientWebApplication/Controllers/FeedbackController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using PatientWebApplication.Models;
+using PatientWebApplication.Services;
 
 namespace PatientWebApplication.Controllers
 {
@@ -16,8 +18,18 @@ namespace PatientWebApplication.Controllers
         public IActionResult Get()
         {
             List<Feedback> result = new List<Feedback>();
-            //Program.Feedback.ForEach(product => result.Add(ProductAdapter.ProductToProductDto(product)));
+            //Program.Feedback.ForEach(product => re sult.Add(ProductAdapter.ProductToProductDto(product)));
             return Ok(Program.Feedback);
         }
+
+        [HttpGet("published")]       // GET /api/feedback/published
+        public IActionResult GetPublished()
+        {
+            List<Feedback> result = FeedbackService.GetPublished();
+            
+            //Program.Feedback.ForEach(product => product.IsPublished ? result.Add(ProductAdapter.ProductToProductDto(product)) : result);
+            return Ok(result);
+        }
+
     }
 }
--- PatientWebApplication/PatientWebApplication/Services/FeedbackService.cs
+++ PatientWebApplication/PatientWebApplication/Services/FeedbackService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PatientWebApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,18 @@ namespace PatientWebApplication.Services
 {
     public static class FeedbackService
     {
+        public static List<Feedback> GetPublished()
+        {
+            List<Feedback> result = new List<Feedback>();
+            foreach (Feedback feedback in Program.Feedback)
+            {
+                if (feedback.IsPublished)
+                {
+                    result.Add(feedback);
+                }
+            }
+
+            return result;
+        }
     }
 }
