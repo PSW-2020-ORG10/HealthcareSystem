c8c0d2892358f9df542c6e9daaa3d5c699a426ae
Nemanja Dimitrijevic <necapro@gmail.com>
2020-11-07 17:14:48 +0100

    feat [#W1.4] : Add date of sending feedback to feedback model

--- PatientWebApplication/PatientWebApplication/Models/MyDbContext.cs
+++ PatientWebApplication/PatientWebApplication/Models/MyDbContext.cs
@@ -98,10 +98,10 @@ namespace PatientWebApplication.Models
 
             modelBuilder.Entity<Feedback>().HasData(
 
-            new Feedback(1, "First message", true, true, 1),
-            new Feedback(2, "Second message", false, false, 1),
-            new Feedback(3, "Third message", true, false, 1),
-            new Feedback(4, "Fourth message", true, false, 1)
+            new Feedback(1, "First message", true, true, new DateTime(2020, 11, 2), 1),
+            new Feedback(2, "Second message", false, false, new DateTime(2019, 6, 23), 1),
+            new Feedback(3, "Third message", true, false, new DateTime(2020, 2, 12), 1),
+            new Feedback(4, "Fourth message", true, false, new DateTime(2020, 9, 11), 1)
 
            );
 
