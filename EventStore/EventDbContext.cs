e9371fe1895a4ece95c3770bc96dbb2007be4c17
velicko-tam <tamara.velickovic98@gmail.com>
2021-01-08 23:13:42 +0100

    #feat[#D]: Updating Event Store logic

--- EventStore/EventDbContext.cs
+++ EventStore/EventDbContext.cs
@@ -10,8 +10,27 @@ namespace EventStore
     public class EventDbContext : DbContext
     {
         public DbSet<FeedbackSubmittedEvent> FeedbackSubmittedEvents { get; set; }
+
+        public EventDbContext() { }
         public EventDbContext(DbContextOptions<EventDbContext> options) : base(options) { }
 
+        private string CreateConnectionStringFromEnvironment()
+        {
+            string server = Environment.GetEnvironmentVariable("DATABASE_HOST") ?? "localhost";
+            string port = Environment.GetEnvironmentVariable("DATABASE_PORT") ?? "3306";
+            string database = Environment.GetEnvironmentVariable("DATABASE_SCHEMA") ?? "EventsDB";
+            string user = Environment.GetEnvironmentVariable("DATABASE_USERNAME") ?? "root";
+            string password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "root";
+
+            return $"server={server};port={port};database={database};user={user};password={password}";
+           
+        }
+
+        public EventDbContext CreateDbContext()
+        {
+            var builder = new DbContextOptionsBuilder<EventDbContext>();
+            return new EventDbContext(builder.UseMySql(CreateConnectionStringFromEnvironment()).Options);
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
