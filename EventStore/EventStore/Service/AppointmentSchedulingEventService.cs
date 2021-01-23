5d3879b7fc3529b24220af2dfbfbd324f4df2655
Aleksa Santrač <akimbo.santrac@gmail.com>
2021-01-23 14:26:37 +0100

    fix [#W5.3] : clean code

--- EventStore/EventStore/Service/AppointmentSchedulingEventService.cs
+++ EventStore/EventStore/Service/AppointmentSchedulingEventService.cs
@@ -79,23 +79,21 @@ namespace EventStore.Service
 
         public CountStepsEventDto GetStatisticsMinStepsForCancelling()
         {
-            List<long> unsuccessfulAppointmentsAttempts = GetCanceledAppointmentsAttempts();
             List<long> successfulAppointmentsAttempts = GetSuccessfulAppointmentsAttempts();
             if (successfulAppointmentsAttempts.Count > 0)
             {
-                var minEvent = GetAll().Where(appointmentEvent => unsuccessfulAppointmentsAttempts.Except(successfulAppointmentsAttempts).Contains(appointmentEvent.Attempt)).ToList().GroupBy(appointmentEvent => appointmentEvent.Attempt).Select(grp => new { Attempt = grp.Key, Count = grp.Count() - 1 }).ToList().Aggregate((eventWithMinCount, currentEvent) => currentEvent.Count <= eventWithMinCount.Count ? currentEvent : eventWithMinCount);
+                var minEvent = GetAll().Where(appointmentEvent => GetCanceledAppointmentsAttempts().Except(successfulAppointmentsAttempts).Contains(appointmentEvent.Attempt)).ToList().GroupBy(appointmentEvent => appointmentEvent.Attempt).Select(grp => new { Attempt = grp.Key, Count = grp.Count() - 1 }).ToList().Aggregate((eventWithMinCount, currentEvent) => currentEvent.Count <= eventWithMinCount.Count ? currentEvent : eventWithMinCount);
                 return new CountStepsEventDto(GetEventByAttempt(minEvent.Attempt), minEvent.Count);
             }
             return null;
         }
 
         public CountStepsEventDto GetStatisticsMaxStepsForCancelling()
-        {
-            List<long> unsuccessfulAppointmentsAttempts = GetCanceledAppointmentsAttempts();
+        {          
             List<long> successfulAppointmentsAttempts = GetSuccessfulAppointmentsAttempts();
             if (successfulAppointmentsAttempts.Count > 0)
             {
-                var maxEvent = GetAll().Where(appointmentEvent => unsuccessfulAppointmentsAttempts.Except(successfulAppointmentsAttempts).Contains(appointmentEvent.Attempt)).ToList().GroupBy(appointmentEvent => appointmentEvent.Attempt).Select(grp => new { Attempt = grp.Key, Count = grp.Count() - 1 }).ToList().Aggregate((eventWithMaxCount, currentEvent) => currentEvent.Count >= eventWithMaxCount.Count ? currentEvent : eventWithMaxCount);
+                var maxEvent = GetAll().Where(appointmentEvent => GetCanceledAppointmentsAttempts().Except(successfulAppointmentsAttempts).Contains(appointmentEvent.Attempt)).ToList().GroupBy(appointmentEvent => appointmentEvent.Attempt).Select(grp => new { Attempt = grp.Key, Count = grp.Count() - 1 }).ToList().Aggregate((eventWithMaxCount, currentEvent) => currentEvent.Count >= eventWithMaxCount.Count ? currentEvent : eventWithMaxCount);
                 return new CountStepsEventDto(GetEventByAttempt(maxEvent.Attempt), maxEvent.Count);
             }
             return null;
@@ -136,30 +134,18 @@ namespace EventStore.Service
             return GetSuccessfulAppointmentsAttempts().Count > 0 ? (double)GetAll().Where(appointmentEvent => GetSuccessfulAppointmentsAttempts().Contains(appointmentEvent.Attempt)).ToList().GroupBy(appointmentEvent => appointmentEvent.Attempt).ToList().Select(item => (item.ToList().SingleOrDefault(listItem => listItem.Action.Equals("create")).TimeStamp - item.ToList().SingleOrDefault(listItem => listItem.Action.Equals("start")).TimeStamp).TotalSeconds).Sum() / (double)GetSuccessfulAppointmentsAttempts().Count : 0;
         }
         public int GetHowManySchedulesInMinimumSteps()
-        {
-            if (GetSuccessfulAppointmentsAttempts().Count > 0)
-            {
-                return GetAll().Where(appointmentEvent => GetSuccessfulAppointmentsAttempts().Contains(appointmentEvent.Attempt)).ToList().GroupBy(appointmentEvent => appointmentEvent.Attempt).Where(grp => grp.Count() - 2 == 4).ToList().Count();
-            }
-            return 0;
+        {                  
+            return (GetSuccessfulAppointmentsAttempts().Count > 0) ? GetAll().Where(appointmentEvent => GetSuccessfulAppointmentsAttempts().Contains(appointmentEvent.Attempt)).ToList().GroupBy(appointmentEvent => appointmentEvent.Attempt).Where(grp => grp.Count() - 2 == 4).ToList().Count() : 0;                     
         }
 
         public int GetHowManySchedulesInMediumSteps()
         {
-            if (GetSuccessfulAppointmentsAttempts().Count > 0)
-            {
-                return GetAll().Where(appointmentEvent => GetSuccessfulAppointmentsAttempts().Contains(appointmentEvent.Attempt)).ToList().GroupBy(appointmentEvent => appointmentEvent.Attempt).Where(grp => grp.Count() - 2 > 4 && grp.Count() - 2 <= 7).ToList().Count();
-            }
-            return 0;
+            return (GetSuccessfulAppointmentsAttempts().Count > 0) ? GetAll().Where(appointmentEvent => GetSuccessfulAppointmentsAttempts().Contains(appointmentEvent.Attempt)).ToList().GroupBy(appointmentEvent => appointmentEvent.Attempt).Where(grp => grp.Count() - 2 > 4 && grp.Count() - 2 <= 7).ToList().Count() : 0;
         }
 
         public int GetHowManySchedulesInMoreSteps()
         {
-            if (GetSuccessfulAppointmentsAttempts().Count > 0)
-            {
-                return GetAll().Where(appointmentEvent => GetSuccessfulAppointmentsAttempts().Contains(appointmentEvent.Attempt)).ToList().GroupBy(appointmentEvent => appointmentEvent.Attempt).Where(grp => grp.Count() - 2 > 7).ToList().Count();
-            }
-            return 0;
+            return (GetSuccessfulAppointmentsAttempts().Count > 0) ? GetAll().Where(appointmentEvent => GetSuccessfulAppointmentsAttempts().Contains(appointmentEvent.Attempt)).ToList().GroupBy(appointmentEvent => appointmentEvent.Attempt).Where(grp => grp.Count() - 2 > 7).ToList().Count() : 0;        
         }
 
         private List<long> GetSuccessfulAppointmentsAttempts()
